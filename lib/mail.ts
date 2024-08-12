a42811c0defd7c063477b298a166b801d093b3c8
Salman Abo Araj <salmanaboaraj@gmail.com>
2024-08-13 02:34:55 +0300

    emaill

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,51 +1,37 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
-const transporter = nodemailer.createTransport({
-  // host: "smtp.gmail.com",
-  service: "hotmail",
-  host: "smtp-mail.outlook.com",
-  port: 587,
-  pool: true,
-  secure: false, // true for 465, false for other ports
-  auth: {
-    user: "salman.aboaraj@outlook.com",
-    pass: "Mirnaaboaraj93",
-  },
-  tls: {
-    // do not fail on invalid certs
-    //rejectUnauthorized: false,
-    ciphers: "SSLv3",
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendVerificationEmail(email: string, token: string) {
-  console.log("sss");
-  const verificationUrl = `https://alwathba.vercel.app/api/verification/${token}`; // Nowy format URL
-  await transporter.sendMail({
-    from: '"AlWathba" <salman.aboaraj@outlook.com>',
-    to: email,
-    subject: "Verify Your Email",
-    html: `Please click on the following link to verify your email: <a href="${verificationUrl}">${verificationUrl}</a>`,
-  });
+  const verificationUrl = `https://alwathba.vercel.app/api/verification/${token}`;
+
+  try {
+    const response = await resend.emails.send({
+      from: "alwathba <onboarding@resend.dev>",
+      to: email,
+      subject: "Verify Your Email",
+      html: `Please click on the following link to verify your email: <a href="${verificationUrl}">${verificationUrl}</a>`,
+    });
+    console.log("Verification email sent:", response);
+  } catch (error) {
+    console.error("Error sending verification email:", error);
+  }
 }
 
 export async function sendPasswordResetEmail(email: string, token: string) {
   const resetPasswordUrl = `https://alwathba.vercel.app/api/resetpass/${encodeURIComponent(
     token
   )}`;
-  await transporter.sendMail({
-    from: '"AlWathba" <salman.aboaraj@outlook.com>',
-    to: email,
-    subject: "Password Reset Request",
-    html: `We received a request to reset your password for our app. Please click on the following link to reset your password: <a href="${resetPasswordUrl}">Reset Password</a>. If you did not request a password reset, please ignore this email.`,
-  });
-}
 
-// export async function sendNewPasswordEmail(email: string, newPassword: string) {
-//   await transporter.sendMail({
-//     from: '"Your App Name" <salman5577@hotmail.com>',
-//     to: email,
-//     subject: "Your New Password",
-//     html: `Your password has been reset. Here is your new password: <strong>${newPassword}</strong>. It is recommended to change this password after logging in.`,
-//   });
-// }
+  try {
+    const response = await resend.emails.send({
+      from: "alwathba <onboarding@resend.dev>",
+      to: email,
+      subject: "Password Reset Request",
+      html: `We received a request to reset your password for our app. Please click on the following link to reset your password: <a href="${resetPasswordUrl}">Reset Password</a>. If you did not request a password reset, please ignore this email.`,
+    });
+    console.log("Password reset email sent:", response);
+  } catch (error) {
+    console.error("Error sending password reset email:", error);
+  }
+}
